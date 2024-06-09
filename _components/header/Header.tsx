fbe7aa6da42b1241ca38fe415b72e9c2b0f1178c
yuvraj <yuvrajgupta468@gmail.com>
2024-06-09 13:11:48 +0530

    logo change

--- _components/header/Header.tsx
+++ _components/header/Header.tsx
@@ -13,7 +13,7 @@ const Header = () => {
               alt="Flowbite Logo"
             />
             <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
-              Flowbite
+              Coloni
             </span>
           </a>
           <div className="flex items-center lg:order-2">
